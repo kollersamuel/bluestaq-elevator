e66248aa3d4ee8bbce022b8ec6103fb7d1a897d1
kollers <samuel.a.koller@gmail.com>
2024-10-17 20:08:46 -0600

    Elevator queues stops correctly

--- src/classes/elevator.py
+++ src/classes/elevator.py
@@ -7,10 +7,12 @@ Updated: 17 October 2024
 Class for the Elevator object, which tracks the state an contents of the elevator.
 """
 
+import bisect
 from enum import Enum
 
 
 class Status(Enum):
+    """Possible Statuses of the Elevator."""
     IDLE = "Idle"
     UP = "Up"
     DOWN = "Down"
@@ -33,71 +35,40 @@ class Elevator:
         """Initializes an Elevator class, sets initial state to Idle."""
         self.status: str = Status.IDLE
         self.current_floor: int = 1
-        self.direction_up: bool | None = None
+        self.direction_up: bool = True
         self.stop_queue: list[int] = []
-        self.top_floor: int = 5  # Temporary assumption.
+        self.top_floor: int = 20  # Temporary assumption.
 
     def add_stop(self, requested_floor: int) -> None:
-        # If there are no scheduled stops, add the requested floor and set the direction.
-        if not self.stop_queue:
-            if requested_floor > self.current_floor:
-                self.direction_up = True
-            elif requested_floor < self.current_floor:
-                self.direction_up = False
-            else:
-                return
-            self.stop_queue = [requested_floor]
-        # If the elevator is already going up, insert the new request such that it falls chronologically in the
-        # ascending sequence if the floor is above or in the descending sequence if the floor is below.
-        elif self.direction_up:
-            if requested_floor > self.current_floor:
-                index_to_insert = next(
-                    (
-                        i
-                        for i, v in enumerate(self.stop_queue)
-                        if v < requested_floor or v < self.current_floor
-                    ),
-                    None,
-                )
-                self.stop_queue.insert(index_to_insert, requested_floor)
-            elif requested_floor < self.current_floor:
-                index_to_insert = next(
-                    (
-                        i
-                        for i, v in enumerate(self.stop_queue)
-                        if v > requested_floor or v > self.current_floor
-                    ),
-                    None,
-                )
-                self.stop_queue.insert(index_to_insert, requested_floor)
-            else:
-                return
-        # If the elevator is already going down, insert the new request such that it falls chronologically in the
-        # ascending sequence if the floor is above or in the descending sequence if the floor is below.
-        elif self.direction_up == False:
-            if requested_floor > self.current_floor:
-                index_to_insert = next(
-                    (
-                        i
-                        for i, v in enumerate(reversed(self.stop_queue))
-                        if v < requested_floor or v < self.current_floor
-                    ),
-                    None,
-                )
-                self.stop_queue.insert(index_to_insert, requested_floor)
-            elif requested_floor < self.current_floor:
-                index_to_insert = next(
-                    (
-                        i
-                        for i, v in enumerate(reversed(self.stop_queue))
-                        if v > requested_floor or v > self.current_floor
-                    ),
-                    None,
-                )
-                self.stop_queue.insert(index_to_insert, requested_floor)
-            else:
-                return
-        # Else catches a request to the current floor, and ignores.
+        """
+        Adds the requested floor the the priority queue in the correct position.
+
+        Ignores the request if the floor does not exist or the request is for the current floor.
+
+        Parameters:
+            requested_floor (int): The floor to add to the priority queue.
+        """
+        # Catch a request to the current floor, and open door.
         # TODO: Have this open doors.
-        else:
+        if requested_floor == self.current_floor:
             return
+        if requested_floor <= 0 or requested_floor > self.top_floor:
+            return
+        elif self.stop_queue:
+            if requested_floor in self.stop_queue:
+                return
+            self.stop_queue.append(requested_floor)
+        else:
+            self.stop_queue = [requested_floor]
+
+        self.stop_queue.sort()
+        split_index = bisect.bisect_left(self.stop_queue, self.current_floor)
+
+        if self.direction_up:
+            on_way = self.stop_queue[split_index:]
+            on_return = list(reversed(self.stop_queue[:split_index]))
+        else:
+            on_way = list(reversed(self.stop_queue[:split_index]))
+            on_return = self.stop_queue[split_index:]
+
+        self.stop_queue = on_way + on_return
